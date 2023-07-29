a2f3802cfb45d6ddd22a620b0f1ca420ac673a95
Dennis Serrano <105755769+Dennis-SB@users.noreply.github.com>
2023-07-29 10:27:40 -0600

    Se Agrega Script de SPs GetAll

--- SQL/GetAll_SPs.sql
+++ SQL/GetAll_SPs.sql
@@ -1,7 +1,6 @@
 
 USE [XtremeAutoNetCore]
 GO
-/****** Object:  StoredProcedure [dbo].[sp_GetAllRoles]    Script Date: 6/18/2023 9:29:29 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -12,38 +11,28 @@ GO
 -- Description:	<Description,,>
 -- =============================================
 ALTER PROCEDURE [dbo].[sp_GetAllRoles]
-	-- Add the parameters for the stored procedure here
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-
-    -- Insert statements for procedure here
 	SELECT * from Rol
 END
 
 USE [XtremeAutoNetCore]
 GO
-/****** Object:  StoredProcedure [dbo].[sp_GetAllCarroModelos]    Script Date: 6/18/2023 9:29:29 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+		
 -- =============================================
 -- Author:		<Author,,Name>
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_GetAllCarroModelos]
-	-- Add the parameters for the stored procedure here
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-
-    -- Insert statements for procedure here
 	SELECT * from CarroModelo
 END
 
@@ -53,23 +42,19 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+		
 -- =============================================
 -- Author:		<Author,,Name>
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_GetAllCarroVendidos]
-	-- Add the parameters for the stored procedure here
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-
-    -- Insert statements for procedure here
 	SELECT * from CarroVendido
 END
-
+		
 USE [XtremeAutoNetCore]
 GO
 SET ANSI_NULLS ON
@@ -82,14 +67,9 @@ GO
 -- Description:	<Description,,>
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_GetAllColores]
-	-- Add the parameters for the stored procedure here
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-
-    -- Insert statements for procedure here
 	SELECT * from Color
 END
 
@@ -99,20 +79,16 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+		
 -- =============================================
 -- Author:		<Author,,Name>
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_GetAllRuedas]
-	-- Add the parameters for the stored procedure here
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-
-    -- Insert statements for procedure here
 	SELECT * from Rueda
 END
 
@@ -128,14 +104,9 @@ GO
 -- Description:	<Description,,>
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_GetAllSeguros]
-	-- Add the parameters for the stored procedure here
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-
-    -- Insert statements for procedure here
 	SELECT * from Seguro
 END
 
@@ -151,14 +122,9 @@ GO
 -- Description:	<Description,,>
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_GetAllTarjetas]
-	-- Add the parameters for the stored procedure here
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-
-    -- Insert statements for procedure here
 	SELECT * from Tarjeta
 END
 
