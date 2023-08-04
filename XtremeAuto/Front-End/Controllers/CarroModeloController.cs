ae590d6215d68f1965e8dbbafbf50acc1820e3ea
jvasquezb20 <jvaquez40413@ufide.ac.cr>
2023-08-04 14:31:34 -0600

    FE basico,controllers,helpers,vista

---
+++ XtremeAuto/Front-End/Controllers/CarroModeloController.cs
@@ -0,0 +1,95 @@
+﻿using Front_End.Helpers;
+using Front_End.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Front_End.Controllers
+{
+    public class CarroModeloController : Controller
+    {
+        CarroModeloHelper carroModeloHelper;
+
+
+
+        // GET: CarroModeloController
+        public ActionResult Index()
+        {
+            carroModeloHelper = new CarroModeloHelper();
+           
+            List<CarroModeloViewModel> list = carroModeloHelper.GetAll();
+
+            return View(list);
+
+            return View();
+        }
+
+        // GET: CarroModeloController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CarroModeloController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CarroModeloController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarroModeloController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CarroModeloController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarroModeloController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CarroModeloController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ XtremeAuto/Front-End/Helpers/CarroModeloHelper.cs
@@ -0,0 +1,118 @@
+﻿using Front_End.Models;
+using Newtonsoft.Json;
+
+namespace Front_End.Helpers
+{
+    public class CarroModeloHelper
+    {
+        ServiceRepository repository;
+
+        public CarroModeloHelper()
+        {
+            repository = new ServiceRepository();
+        }
+
+        #region  GetALL
+
+        public List<CarroModeloViewModel> GetAll()
+        {
+
+
+
+            List<CarroModeloViewModel> lista = new List<CarroModeloViewModel>();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/CarroModelo/");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<CarroModeloViewModel>>(content);
+
+            }
+
+
+
+
+            return lista;
+        }
+        #endregion
+
+
+        #region GetByID
+
+        /// <summary>
+        /// Obtener CarroModelo por ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public CarroModeloViewModel GetByID(int id)
+        {
+            CarroModeloViewModel carroModelo = new CarroModeloViewModel();
+
+            HttpResponseMessage responseMessage = repository.GetResponse("api/carroModelo/" + id);
+            string content = responseMessage.Content.ReadAsStringAsync().Result;
+            carroModelo = JsonConvert.DeserializeObject<CarroModeloViewModel>(content);
+
+            return carroModelo;
+
+        }
+
+
+
+        #endregion
+
+
+        #region Update
+        public CarroModeloViewModel Edit(CarroModeloViewModel carromodelo)
+        {
+
+            HttpResponseMessage responseMessage = repository.PutResponse("api/carroModelo/", carromodelo);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            CarroModeloViewModel categoryAPI = JsonConvert.DeserializeObject<CarroModeloViewModel>(content);
+            return categoryAPI;
+
+        }
+
+
+        #endregion
+
+
+        #region Create
+        public CarroModeloViewModel Add(CarroModeloViewModel category)
+        {
+
+            HttpResponseMessage responseMessage = repository.PostResponse("api/carroModelo/", category);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            CarroModeloViewModel carroModeloAPI = JsonConvert.DeserializeObject<CarroModeloViewModel>(content);
+            return carroModeloAPI;
+
+        }
+
+
+        #endregion
+
+
+        #region GetByID
+
+        /// <summary>
+        /// Obtener Categoria por ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public CarroModeloViewModel Delete(int id)
+        {
+            CarroModeloViewModel category = new CarroModeloViewModel();
+
+            HttpResponseMessage responseMessage = repository.DeleteResponse("api/carroModelo/" + id);
+            // string content = responseMessage.Content.ReadAsStringAsync().Result;
+            // category = JsonConvert.DeserializeObject<CategoryViewModel>(content);
+
+            return category;
+
+        }
+
+
+
+        #endregion
+
+    }
+}
+
