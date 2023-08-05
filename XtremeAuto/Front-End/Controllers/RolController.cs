6b0ff99cb6e328eb1cead9374e1612baed1eb4c3
jvasquezb20 <jvaquez40413@ufide.ac.cr>
2023-08-05 16:49:18 -0600

    Controller de Rol

---
+++ XtremeAuto/Front-End/Controllers/RolController.cs
@@ -0,0 +1,90 @@
+﻿using Front_End.Helpers;
+using Front_End.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Front_End.Controllers
+{
+    public class RolController : Controller
+    {
+        private RolHelper rolHelper;
+
+        // GET: RolController
+        public ActionResult Index()
+        {
+            rolHelper = new RolHelper();
+
+            List<RolViewModel> list = rolHelper.GetAll();
+
+            return View(list);
+        }
+        // GET: RolController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RolController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RolController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RolController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RolController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RolController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RolController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ XtremeAuto/Front-End/Helpers/RolHelper.cs
@@ -0,0 +1,98 @@
+﻿using Front_End.Models;
+using Newtonsoft.Json;
+
+namespace Front_End.Helpers
+{
+    public class RolHelper
+    {
+        ServiceRepository repository;
+
+        public RolHelper()
+        {
+            repository = new ServiceRepository();
+        }
+
+        #region  GetALL
+
+        public List<RolViewModel> GetAll()
+        {
+
+
+
+            List<RolViewModel> lista = new List<RolViewModel>();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Rol/");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<RolViewModel>>(content);
+
+            }
+
+            return lista;
+        }
+        #endregion
+
+
+        #region GetByID
+        public RolViewModel GetByID(int id)
+        {
+            RolViewModel rol = new RolViewModel();
+
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Rol/" + id);
+            string content = responseMessage.Content.ReadAsStringAsync().Result;
+            rol = JsonConvert.DeserializeObject<RolViewModel>(content);
+
+            return rol;
+
+        }
+
+
+
+        #endregion
+
+
+        #region Update
+        public RolViewModel Edit(RolViewModel rol)
+        {
+
+            HttpResponseMessage responseMessage = repository.PutResponse("api/rol/", rol);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            RolViewModel rolAPI = JsonConvert.DeserializeObject<RolViewModel>(content);
+            return rolAPI;
+
+        }
+
+
+        #endregion
+
+
+        #region Create
+        public RolViewModel Add(RolViewModel rol)
+        {
+
+            HttpResponseMessage responseMessage = repository.PostResponse("api/rol/", rol);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            RolViewModel rolAPI = JsonConvert.DeserializeObject<RolViewModel>(content);
+            return rolAPI;
+
+        }
+
+
+        #endregion
+
+
+        #region GetByID
+        public RolViewModel Delete(int id)
+        {
+            RolViewModel rol = new RolViewModel();
+
+            HttpResponseMessage responseMessage = repository.DeleteResponse("api/rueda/" + id);
+            // string content = responseMessage.Content.ReadAsStringAsync().Result;
+            // category = JsonConvert.DeserializeObject<CategoryViewModel>(content);
+
+            return rol;
+        }
+        #endregion
+
+    }
+}
