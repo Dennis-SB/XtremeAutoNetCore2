a02bbabb58e9c871373bbcf2bae232352e2f5413
Carlos4708 <educoncal@hotmail.com>
2023-08-07 21:53:28 -0600

    Se corrigen las regiones de la clase TransaccionController

--- XtremeAuto/BackEnd/Controllers/TransaccionController.cs
+++ XtremeAuto/BackEnd/Controllers/TransaccionController.cs
@@ -12,9 +12,7 @@ namespace BackEnd.Controllers
     [ApiController]
     public class TransaccionController : ControllerBase
     {
-
         private ITransaccionDAL transaccionDAL;
-
         private TransaccionModel Convertir(Transaccion transaccion)
         {
             return new TransaccionModel
@@ -27,12 +25,8 @@ namespace BackEnd.Controllers
                 InteresesMorosidad = transaccion.InteresesMorosidad,
                 Pagado = transaccion.Pagado,
                 Precio = transaccion.Precio
-
             };
         }
-
-
-
         private Transaccion Convertir(TransaccionModel transaccion)
         {
             return new Transaccion
@@ -48,34 +42,24 @@ namespace BackEnd.Controllers
             };
         }
 
-
         #region Constructores
-
         public TransaccionController()
         {
             transaccionDAL = new TransaccionDALImpl();
-
         }
-
         #endregion
 
-
         #region Consultas
-
         // GET: api/<TransaccionController>
         [HttpGet]
         public async Task<JsonResult> Get()
         {
             IEnumerable<Transaccion> transacciones = await transaccionDAL.GetAll();
             List<TransaccionModel> models = new List<TransaccionModel>();
-
             foreach (var transaccion in transacciones)
             {
-
                 models.Add(Convertir(transaccion));
-
             }
-
             return new JsonResult(models);
         }
 
@@ -84,29 +68,21 @@ namespace BackEnd.Controllers
         public async Task<JsonResult> Get(int id)
         {
             Transaccion transaccion = await transaccionDAL.Get(id);
-
-
             return new JsonResult(Convertir(transaccion));
         }
         #endregion
 
         #region Agregar
-
-
         // POST api/<TransaccionController>
         [HttpPost]
         public JsonResult Post([FromBody] TransaccionModel transaccion)
         {
-
             transaccionDAL.Add(Convertir(transaccion));
             return new JsonResult(transaccion);
         }
-
         #endregion
 
         #region Modificar
-
-
         // PUT api/<TransaccionController>/5
         [HttpPut]
         public JsonResult Put([FromBody] TransaccionModel transaccion)
@@ -115,7 +91,6 @@ namespace BackEnd.Controllers
             return new JsonResult(transaccion);
         }
         #endregion
-
 
         #region Eliminar
         // DELETE api/<TransaccionController>/5
@@ -126,12 +101,8 @@ namespace BackEnd.Controllers
             {
                 TransaccionId = id
             };
-
             transaccionDAL.Remove(transaccion);
-
         }
-
         #endregion
-
     }
 }
