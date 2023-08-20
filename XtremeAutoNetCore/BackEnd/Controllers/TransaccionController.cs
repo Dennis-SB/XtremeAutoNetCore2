3bc885a4e5e31903dc98457a9297f151c557e9d7
Dennis Serrano <dserrano10115@ufide.ac.cr>
2023-08-20 17:25:29 -0600

    Transaccion BE Complete

---
+++ XtremeAutoNetCore/BackEnd/Controllers/TransaccionController.cs
@@ -0,0 +1,110 @@
+﻿using BackEnd.Models;
+using DAL.Implementations;
+using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransaccionController : Controller
+    {
+        private ITransaccionDAL entidadDAL;
+
+        private TransaccionModel Convertir(Transaccion entidad)
+        {
+            return new TransaccionModel
+            {
+                TransaccionId = entidad.TransaccionId,
+                VentaId = entidad.VentaId,
+                TarjetaId = entidad.TarjetaId,
+                FechaTransaccion = entidad.FechaTransaccion,
+                FechaCorte = entidad.FechaCorte,
+                InteresesMorosidad = entidad.InteresesMorosidad,
+                Pagado = entidad.Pagado,
+                Precio = entidad.Precio
+            };
+        }
+
+        private Transaccion Convertir(TransaccionModel entidad)
+        {
+            return new Transaccion
+            {
+                TransaccionId = entidad.TransaccionId,
+                VentaId = entidad.VentaId,
+                TarjetaId = entidad.TarjetaId,
+                FechaTransaccion = entidad.FechaTransaccion,
+                FechaCorte = entidad.FechaCorte,
+                InteresesMorosidad = entidad.InteresesMorosidad,
+                Pagado = entidad.Pagado,
+                Precio = entidad.Precio
+            };
+        }
+
+        #region Constructores
+        public TransaccionController()
+        {
+            entidadDAL = new TransaccionDALImpl();
+        }
+        #endregion
+
+        #region Consultas
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            IEnumerable<Transaccion> entidades = entidadDAL.GetAll();
+            List<TransaccionModel> models = new List<TransaccionModel>();
+            foreach (var entidad in entidades)
+            {
+                models.Add(Convertir(entidad));
+            }
+            return new JsonResult(models);
+        }
+
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            Transaccion entidad = entidadDAL.Get(id);
+            return new JsonResult(Convertir(entidad));
+        }
+        #endregion
+
+        #region Agregar
+        // POST api/<CategoryController>
+        [HttpPost]
+        public JsonResult Post([FromBody] TransaccionModel entidad)
+        {
+            entidadDAL.Add(Convertir(entidad));
+            return new JsonResult(entidad);
+        }
+        #endregion
+
+        #region Modificar
+        // PUT api/<CategoryController>/5
+        [HttpPut]
+        public JsonResult Put([FromBody] TransaccionModel entidad)
+        {
+            entidadDAL.Update(Convertir(entidad));
+            return new JsonResult(entidad);
+        }
+        #endregion
+
+        #region Eliminar
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Transaccion entidad = new Transaccion
+            {
+                TransaccionId = id
+            };
+            entidadDAL.Remove(entidad);
+        }
+        #endregion
+    }
+}
---
+++ XtremeAutoNetCore/DAL/Implementations/TransaccionDALImpl.cs
@@ -0,0 +1,108 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class TransaccionDALImpl : ITransaccionDAL
+    {
+        private XtremeAutoNetCore2Context Context;
+        private UnidadDeTrabajo<Transaccion> unidad;
+
+        public bool Add(Transaccion entidad)
+        {
+            try
+            {
+                using (unidad = new UnidadDeTrabajo<Transaccion>(new XtremeAutoNetCore2Context()))
+                {
+                    unidad.genericDAL.Add(entidad);
+                    unidad.Complete();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void AddRange(IEnumerable<Transaccion> entidades)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Transaccion> Find(Expression<Func<Transaccion, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Transaccion Get(int id)
+        {
+            Transaccion entidad = null;
+            using (unidad = new UnidadDeTrabajo<Transaccion>(new XtremeAutoNetCore2Context()))
+            {
+                entidad = unidad.genericDAL.Get(id);
+            }
+            return entidad;
+        }
+
+        public IEnumerable<Transaccion> GetAll()
+        {
+            IEnumerable<Transaccion> entidades = null;
+            using (unidad = new UnidadDeTrabajo<Transaccion>(new XtremeAutoNetCore2Context()))
+            {
+                entidades = unidad.genericDAL.GetAll();
+            }
+            return entidades;
+        }
+
+        public bool Remove(Transaccion entidad)
+        {
+            try
+            {
+                using (unidad = new UnidadDeTrabajo<Transaccion>(new XtremeAutoNetCore2Context()))
+                {
+                    unidad.genericDAL.Remove(entidad);
+                    unidad.Complete();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void RemoveRange(IEnumerable<Transaccion> entidades)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Transaccion SingleOrDefault(Expression<Func<Transaccion, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Transaccion entidad)
+        {
+            try
+            {
+                using (unidad = new UnidadDeTrabajo<Transaccion>(new XtremeAutoNetCore2Context()))
+                {
+                    unidad.genericDAL.Update(entidad);
+                    unidad.Complete();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
