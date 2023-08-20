11164eb1c60143c709a6deb718725800ca820d15
Dennis Serrano <dserrano10115@ufide.ac.cr>
2023-08-20 14:47:36 -0600

    Usuario FE Complete

---
+++ XtremeAutoNetCore/FrontEnd/Controllers/UsuarioController.cs
@@ -0,0 +1,117 @@
+﻿using FrontEnd.Helpers;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        private UsuarioHelper entidadHelper;
+        private RolHelper enlace1Helper;
+
+        #region Constructores
+        public UsuarioController()
+        {
+            entidadHelper = new UsuarioHelper();
+            enlace1Helper = new RolHelper();
+        }
+        #endregion
+
+        #region Index
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            List<UsuarioViewModel> list = entidadHelper.GetAll();
+            return View(list);
+        }
+        #endregion
+
+        #region Details
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            UsuarioViewModel entidad = entidadHelper.GetByID(id);
+            return View(entidad);
+        }
+        #endregion
+
+        #region Create
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            UsuarioViewModel entidad = new UsuarioViewModel();
+            entidad.Roles = enlace1Helper.GetAll();
+            return View(entidad);
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UsuarioViewModel entidad)
+        {
+            try
+            {
+                entidad = entidadHelper.Add(entidad);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+
+        #region Edit
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            UsuarioViewModel entidad = entidadHelper.GetByID(id);
+            entidad.Roles = enlace1Helper.GetAll();
+            return View(entidad);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UsuarioViewModel entidad)
+        {
+            try
+            {
+                entidad = entidadHelper.Edit(entidad);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+
+        #region Delete
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            UsuarioViewModel entidad = entidadHelper.GetByID(id);
+            return View(entidad);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(UsuarioViewModel entidad)
+        {
+            try
+            {
+                entidadHelper.Delete(entidad.UsuarioId);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+    }
+}
---
+++ XtremeAutoNetCore/FrontEnd/Helpers/UsuarioHelper.cs
@@ -0,0 +1,69 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class UsuarioHelper
+    {
+        ServiceRepository repository;
+
+        public UsuarioHelper()
+        {
+            repository = new ServiceRepository();
+        }
+
+        #region  GetALL
+        public List<UsuarioViewModel> GetAll()
+        {
+            List<UsuarioViewModel> lista = new List<UsuarioViewModel>();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Usuario/");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<UsuarioViewModel>>(content);
+            }
+            return lista;
+        }
+        #endregion
+
+        #region GetByID
+        public UsuarioViewModel GetByID(int id)
+        {
+            UsuarioViewModel entidad = new UsuarioViewModel();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Usuario/" + id);
+            string content = responseMessage.Content.ReadAsStringAsync().Result;
+            entidad = JsonConvert.DeserializeObject<UsuarioViewModel>(content);
+            return entidad;
+        }
+        #endregion
+
+        #region Update
+        public UsuarioViewModel Edit(UsuarioViewModel entidad)
+        {
+            HttpResponseMessage responseMessage = repository.PutResponse("api/Usuario/", entidad);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            UsuarioViewModel entidadAPI = JsonConvert.DeserializeObject<UsuarioViewModel>(content);
+            return entidadAPI;
+        }
+        #endregion
+
+        #region Create
+        public UsuarioViewModel Add(UsuarioViewModel entidad)
+        {
+            HttpResponseMessage responseMessage = repository.PostResponse("api/Usuario/", entidad);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            UsuarioViewModel entidadAPI = JsonConvert.DeserializeObject<UsuarioViewModel>(content);
+            return entidadAPI;
+        }
+        #endregion
+
+        #region GetByID
+        public UsuarioViewModel Delete(int id)
+        {
+            UsuarioViewModel entidad = new UsuarioViewModel();
+            HttpResponseMessage responseMessage = repository.DeleteResponse("api/Usuario/" + id);
+            return entidad;
+        }
+        #endregion
+    }
+}
