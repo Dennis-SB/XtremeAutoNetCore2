39058af9550b34fa380d164ecfbc3e981524d665
Dennis Serrano <dserrano10115@ufide.ac.cr>
2023-08-20 17:15:12 -0600

    Ventum FE Complete

---
+++ XtremeAutoNetCore/FrontEnd/Controllers/VentumController.cs
@@ -0,0 +1,120 @@
+﻿using FrontEnd.Helpers;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class VentumController : Controller
+    {
+        private VentumHelper entidadHelper;
+        private UsuarioHelper enlace1Helper;
+        private CarroVendidoHelper enlace2Helper;
+
+        #region Constructores
+        public VentumController()
+        {
+            entidadHelper = new VentumHelper();
+            enlace1Helper = new UsuarioHelper();
+            enlace2Helper = new CarroVendidoHelper();
+        }
+        #endregion
+
+        #region Index
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            List<VentumViewModel> list = entidadHelper.GetAll();
+            return View(list);
+        }
+        #endregion
+
+        #region Details
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            VentumViewModel entidad = entidadHelper.GetByID(id);
+            return View(entidad);
+        }
+        #endregion
+
+        #region Create
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            VentumViewModel entidad = new VentumViewModel();
+            entidad.Usuarios = enlace1Helper.GetAll();
+            entidad.CarroVendidos = enlace2Helper.GetAll();
+            return View(entidad);
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(VentumViewModel entidad)
+        {
+            try
+            {
+                entidad = entidadHelper.Add(entidad);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+
+        #region Edit
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            VentumViewModel entidad = entidadHelper.GetByID(id);
+            entidad.Usuarios = enlace1Helper.GetAll();
+            entidad.CarroVendidos = enlace2Helper.GetAll();
+            return View(entidad);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(VentumViewModel entidad)
+        {
+            try
+            {
+                entidad = entidadHelper.Edit(entidad);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+
+        #region Delete
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            VentumViewModel entidad = entidadHelper.GetByID(id);
+            return View(entidad);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(VentumViewModel entidad)
+        {
+            try
+            {
+                entidadHelper.Delete(entidad.VentaId);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+    }
+}
---
+++ XtremeAutoNetCore/FrontEnd/Helpers/VentumHelper.cs
@@ -0,0 +1,69 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class VentumHelper
+    {
+        ServiceRepository repository;
+
+        public VentumHelper()
+        {
+            repository = new ServiceRepository();
+        }
+
+        #region  GetALL
+        public List<VentumViewModel> GetAll()
+        {
+            List<VentumViewModel> lista = new List<VentumViewModel>();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Ventum/");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<VentumViewModel>>(content);
+            }
+            return lista;
+        }
+        #endregion
+
+        #region GetByID
+        public VentumViewModel GetByID(int id)
+        {
+            VentumViewModel entidad = new VentumViewModel();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Ventum/" + id);
+            string content = responseMessage.Content.ReadAsStringAsync().Result;
+            entidad = JsonConvert.DeserializeObject<VentumViewModel>(content);
+            return entidad;
+        }
+        #endregion
+
+        #region Update
+        public VentumViewModel Edit(VentumViewModel entidad)
+        {
+            HttpResponseMessage responseMessage = repository.PutResponse("api/Ventum/", entidad);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            VentumViewModel entidadAPI = JsonConvert.DeserializeObject<VentumViewModel>(content);
+            return entidadAPI;
+        }
+        #endregion
+
+        #region Create
+        public VentumViewModel Add(VentumViewModel entidad)
+        {
+            HttpResponseMessage responseMessage = repository.PostResponse("api/Ventum/", entidad);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            VentumViewModel entidadAPI = JsonConvert.DeserializeObject<VentumViewModel>(content);
+            return entidadAPI;
+        }
+        #endregion
+
+        #region GetByID
+        public VentumViewModel Delete(int id)
+        {
+            VentumViewModel entidad = new VentumViewModel();
+            HttpResponseMessage responseMessage = repository.DeleteResponse("api/Ventum/" + id);
+            return entidad;
+        }
+        #endregion
+    }
+}
